2416fb9f4829feb1b4d539104a452447080f1eb9
Darryl Gamroth <dgamroth@rubus.ca>
2024-10-08 16:02:28 -0700

    Add documentation

--- example/example.jl
+++ example/example.jl
@@ -1,3 +1,8 @@
+"""
+This file contains an example of a hierarchical state machine implemented using the Hsm.jl library.
+See example.png for a graphical representation of the state machine.
+"""
+
 using Revise
 using JET
 using BenchmarkTools
@@ -9,8 +14,35 @@ include("../src/Hsm.jl")
 using .Hsm
 using ValSplit
 
+# Define the state machine
+mutable struct HsmTest <: Hsm.AbstractHsmStateMachine
+    # Variables used by the AbstractHsmStateMachine interface
+    current::Symbol
+    source::Symbol
+    event::Symbol
+
+    # State machine specific variables. For instance this buffer could be used to store the event data
+    buf::Vector{UInt8}
+    foo::Int
+
+    function HsmTest()
+        sm = new()
+
+        # Initialize the state machine
+        Hsm.initialize!(sm)
+        return sm
+    end
+end
+
+# Implement the AbstractHsmStateMachine interface
+Hsm.current(sm::HsmTest) = sm.current
+Hsm.current!(sm::HsmTest, s::Symbol) = sm.current = s
+Hsm.source(sm::HsmTest) = sm.source
+Hsm.source!(sm::HsmTest, s::Symbol) = sm.source = s
+Hsm.event(sm::HsmTest) = sm.event
+
 # Define all states
-const Top = :Top
+const Top = Hsm.Root
 const State_S = :State_S
 const State_S1 = :State_S1
 const State_S11 = :State_S11
@@ -18,35 +50,7 @@ const State_S2 = :State_S2
 const State_S21 = :State_S21
 const State_S211 = :State_S211
 
-# Define the state machine
-mutable struct HsmTest <: Hsm.AbstractHsmStateMachine
-    current::Symbol
-    source::Symbol
-    event::Symbol
-
-    buf::Vector{UInt8}
-
-    # Define state machine variables
-    foo::Int
-
-    function HsmTest()
-        sm = new()
-        Hsm.initialize(sm)
-        Hsm.on_initial!(sm, Top)
-        #print(\n")
-        return sm
-    end
-end
-
-# Define state machine interface functions
-Hsm.event(sm::HsmTest) = sm.event
-Hsm.current(sm::HsmTest) = sm.current
-Hsm.current!(sm::HsmTest, s::Symbol) = sm.current = s
-Hsm.source(sm::HsmTest) = sm.source
-Hsm.source!(sm::HsmTest, s::Symbol) = sm.source = s
-
-# Define all ancestors for HsmTest
-Hsm.ancestor(sm::HsmTest, ::Val{Top}) = Hsm.root()
+# Implement the AbstractHsmStateMachine ancestor interface for each state
 Hsm.ancestor(sm::HsmTest, ::Val{State_S}) = Top
 Hsm.ancestor(sm::HsmTest, ::Val{State_S1}) = State_S
 Hsm.ancestor(sm::HsmTest, ::Val{State_S11}) = State_S1
@@ -54,32 +58,57 @@ Hsm.ancestor(sm::HsmTest, ::Val{State_S2}) = State_S
 Hsm.ancestor(sm::HsmTest, ::Val{State_S21}) = State_S2
 Hsm.ancestor(sm::HsmTest, ::Val{State_S211}) = State_S21
 
-# @valsplit Hsm.on_entry!(sm::Hsm.AbstractHsmStateMachine, Val(state::Symbol)) = #print($(state)-ENTRY;")
-# @valsplit Hsm.on_exit!(sm::Hsm.AbstractHsmStateMachine, Val(state::Symbol)) = #print($(state)-EXIT;")
+# Define a dispatch function. Custom dispatch functions can be defined for each state machine
+function dispatch!(sm::HsmTest, event)
+    #print("$(event) - ")
+    sm.event = event
+    Hsm.dispatch!(sm)
+    #print("\n")
+end
+
+# Globally override default handlers
+# @valsplit Hsm.on_entry!(sm::Hsm.AbstractHsmStateMachine, Val(state::Symbol)) = #print("$(state)-ENTRY;")
+# @valsplit Hsm.on_exit!(sm::Hsm.AbstractHsmStateMachine, Val(state::Symbol)) = #print("$(state)-EXIT;")
 
 ############
 
-Hsm.on_initial!(sm::HsmTest, state::Val{Top}) =
-    Hsm.transition!(sm, State_S2) do
-        #print($(state)-INIT;")
+function Hsm.on_initial!(sm::HsmTest, state::Val{Top})
+    handled = Hsm.transition!(sm, State_S2) do
+        # Do something on the transition
+        #print("$(state)-INIT;")
         sm.foo = 0
     end
+    #print("\n")
+    return handled
+end
 
 ##############
 
 Hsm.on_initial!(sm::HsmTest, state::Val{State_S}) =
     Hsm.transition!(sm, State_S11) do
-        #print($(state)-INIT;")
+        #print("$(state)-INIT;")
     end
 
+# Example of how to implement on_entry! for a state
+# function Hsm.on_entry!(sm::HsmTest, state::Val{State_S})
+#     # Do something when entering the state
+# end
+
+# Example of how to implement on_exit! for a state
+# function Hsm.on_exit!(sm::HsmTest, state::Val{State_S})
+#     # Do something when exiting the state
+# end
+
 Hsm.on_event!(sm::HsmTest, state::Val{State_S}, event::Val{:Event_E}) =
+    # transition! returns EventHandled if the transition is successful
     Hsm.transition!(sm, State_S11) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 function Hsm.on_event!(sm::HsmTest, state::Val{State_S}, event::Val{:Event_I})
+    # Depending on the guard condition foo, the event can be handled or not
     if sm.foo == 1
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
         sm.foo = 0
         return Hsm.EventHandled
     else
@@ -91,28 +120,28 @@ end
 
 Hsm.on_initial!(sm::HsmTest, state::Val{State_S1}) =
     Hsm.transition!(sm, State_S11) do
-        #print($(state)-INIT;")
+        #print("$(state)-INIT;")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_A}) =
     Hsm.transition!(sm, State_S1) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_B}) =
     Hsm.transition!(sm, State_S11) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_C}) =
     Hsm.transition!(sm, State_S2) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 function Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_D})
     if sm.foo == 0
         return Hsm.transition!(sm, State_S1) do
-            #print($(state)-$(event);")
+            #print("$(state)-$(event);")
             sm.foo = 0
         end
     else
@@ -122,21 +151,20 @@ end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_F}) =
     Hsm.transition!(sm, State_S211) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 function Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_I})
-    #print($(state)-$(event);")
+    #print("$(state)-$(event);")
     return Hsm.EventHandled
 end
 
 #############
 
-# Warning, this style can allocate
 function Hsm.on_event!(sm::HsmTest, state::Val{State_S11}, event::Val{:Event_D})
     if sm.foo == 1
         return Hsm.transition!(sm, State_S1) do
-            #print($(state)-$(event);")
+            #print("$(state)-$(event);")
             sm.foo = 0
         end
     else
@@ -146,34 +174,34 @@ end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S11}, event::Val{:Event_G}) =
     Hsm.transition!(sm, State_S211) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S11}, event::Val{:Event_H}) =
     Hsm.transition!(sm, State_S) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 ######
 
 Hsm.on_initial!(sm::HsmTest, state::Val{State_S2}) =
     Hsm.transition!(sm, State_S211) do
-        #print($(state)-INIT;")
+        #print("$(state)-INIT;")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S2}, event::Val{:Event_C}) =
     Hsm.transition!(sm, State_S1) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S2}, event::Val{:Event_F}) =
     Hsm.transition!(sm, State_S11) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 function Hsm.on_event!(sm::HsmTest, state::Val{State_S2}, event::Val{:Event_I})
     if sm.foo == 0
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
         sm.foo = 1
         return Hsm.EventHandled
     else
@@ -185,68 +213,60 @@ end
 
 Hsm.on_initial!(sm::HsmTest, state::Val{State_S21}) =
     Hsm.transition!(sm, State_S211) do
-        #print($(state)-INIT;")
+        #print("$(state)-INIT;")
     end
 
-function Hsm.on_event!(sm::HsmTest, state::Val{State_S21}, event::Val{:Event_A})
-    # Ok when this transition fires we need the SBE event message information
+Hsm.on_event!(sm::HsmTest, state::Val{State_S21}, event::Val{:Event_A}) =
     Hsm.transition!(sm, State_S21) do
-
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
-end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S21}, event::Val{:Event_B}) =
     Hsm.transition!(sm, State_S211) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S21}, event::Val{:Event_G}) =
     Hsm.transition!(sm, State_S11) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 #############
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S211}, event::Val{:Event_D}) =
     Hsm.transition!(sm, State_S21) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
 Hsm.on_event!(sm::HsmTest, state::Val{State_S211}, event::Val{:Event_H}) =
     Hsm.transition!(sm, State_S) do
-        #print($(state)-$(event);")
+        #print("$(state)-$(event);")
     end
 
-function dispatch!(sm, event)
-    #print($(event) - ")
-    sm.event = event
-    Hsm.dispatch!(sm)
-    #print(\n")
-end
+#############
 
-function test(hsm)
-    dispatch!(hsm, :Event_A)
-    dispatch!(hsm, :Event_B)
-    dispatch!(hsm, :Event_D)
-    dispatch!(hsm, :Event_E)
-    dispatch!(hsm, :Event_I)
-    dispatch!(hsm, :Event_F)
-    dispatch!(hsm, :Event_I)
-    dispatch!(hsm, :Event_I)
-    dispatch!(hsm, :Event_F)
-    dispatch!(hsm, :Event_A)
-    dispatch!(hsm, :Event_B)
-    dispatch!(hsm, :Event_D)
-    dispatch!(hsm, :Event_D)
-    dispatch!(hsm, :Event_E)
-    dispatch!(hsm, :Event_G)
-    dispatch!(hsm, :Event_H)
-    dispatch!(hsm, :Event_H)
-    dispatch!(hsm, :Event_C)
-    dispatch!(hsm, :Event_G)
-    dispatch!(hsm, :Event_C)
-    dispatch!(hsm, :Event_C)
+function test(sm::HsmTest)
+    dispatch!(sm, :Event_A)
+    dispatch!(sm, :Event_B)
+    dispatch!(sm, :Event_D)
+    dispatch!(sm, :Event_E)
+    dispatch!(sm, :Event_I)
+    dispatch!(sm, :Event_F)
+    dispatch!(sm, :Event_I)
+    dispatch!(sm, :Event_I)
+    dispatch!(sm, :Event_F)
+    dispatch!(sm, :Event_A)
+    dispatch!(sm, :Event_B)
+    dispatch!(sm, :Event_D)
+    dispatch!(sm, :Event_D)
+    dispatch!(sm, :Event_E)
+    dispatch!(sm, :Event_G)
+    dispatch!(sm, :Event_H)
+    dispatch!(sm, :Event_H)
+    dispatch!(sm, :Event_C)
+    dispatch!(sm, :Event_G)
+    dispatch!(sm, :Event_C)
+    dispatch!(sm, :Event_C)
 end
 
 hsm = HsmTest()
--- src/Hsm.jl
+++ src/Hsm.jl
@@ -2,36 +2,274 @@ module Hsm
 
 using ValSplit
 
+"""
+    AbstractHsmStateMachine
+
+Interface type for a hierarchical state machine.
+
+Interface definition:
+
+| Required Methods         | Description                                        |
+|--------------------------|----------------------------------------------------|
+| `current(sm)`            | Get current state of state machine `sm`            |
+| `current!(sm, state)`    | Set current state of state machine `sm` to `state` |
+| `source(sm)`             | Get source state of state machine `sm`             |
+| `source!(sm, state)`     | Set source state of state machine `sm` to `state`  |
+| `event(sm)`              | Get event of state machine `sm`                    |
+
+# Example
+```julia
+mutable struct HsmTest <: Hsm.AbstractHsmStateMachine
+    # Variables used by the AbstractHsmStateMachine interface
+    current::Symbol
+    source::Symbol
+    event::Symbol
+
+    # `foo` is an example of a state machine specific variable
+    foo::Int
+
+    function HsmTest()
+        sm = new()
+        # Initialize the state machine
+        Hsm.initialize!(sm)
+        return sm
+    end
+end
+
+# Implement the AbstractHsmStateMachine interface
+Hsm.current(sm::HsmTest) = sm.current
+Hsm.current!(sm::HsmTest, s::Symbol) = sm.current = s
+Hsm.source(sm::HsmTest) = sm.source
+Hsm.source!(sm::HsmTest, s::Symbol) = sm.source = s
+Hsm.event(sm::HsmTest) = sm.event
+
+# Define all states
+const Top = Hsm.Root
+const State_S = :State_S
+const State_S1 = :State_S1
+const State_S11 = :State_S11
+const State_S2 = :State_S2
+const State_S21 = :State_S21
+const State_S211 = :State_S211
+
+# Implement the AbstractHsmStateMachine ancestor interface for each state
+Hsm.ancestor(sm::HsmTest, ::Val{State_S}) = Top
+Hsm.ancestor(sm::HsmTest, ::Val{State_S1}) = State_S
+Hsm.ancestor(sm::HsmTest, ::Val{State_S11}) = State_S1
+Hsm.ancestor(sm::HsmTest, ::Val{State_S2}) = State_S
+Hsm.ancestor(sm::HsmTest, ::Val{State_S21}) = State_S2
+Hsm.ancestor(sm::HsmTest, ::Val{State_S211}) = State_S21
+```
+"""
 abstract type AbstractHsmStateMachine end
 const StateType = Symbol
 
+"""
+    EventReturn
+
+Enumeration of the possible return values from [`on_event!`](@ref) function.
+
+    Returns:
+        EventNotHandled: The event was not handled by the state machine.
+        EventHandled: The event was handled by the state machine.
+"""
 @enum EventReturn EventNotHandled EventHandled
 
+"""
+    Root
+
+Root state of the state machine. Used by [`ancestor`](@ref) to specify the top most state.
+
+# Example
+```julia
+const Top = Hsm.Root
+```
+"""
+const Root = :Root
+
+"""
+    current(sm::AbstractHsmStateMachine)
+
+Get current state of state machine `sm`.
+
+# Implementation Example
+```julia
+Hsm.current(sm::HsmTest) == sm.current
+```
+"""
 function current(::AbstractHsmStateMachine) end
-function current!(::AbstractHsmStateMachine, state::StateType) end
+
+"""
+    current!(sm::AbstractHsmStateMachine, state::StateType)
+
+Set current state of state machine `sm` to `state`.
+
+# Implementation Example
+```julia
+Hsm.current!(sm::HsmTest, state) = sm.current = state
+```
+"""
+function current!(::AbstractHsmStateMachine, ::StateType) end
+
+"""
+    source(sm::AbstractHsmStateMachine)
+
+Get source state of state machine `sm`.
+
+# Implementation Example
+```julia
+Hsm.source(sm::HsmTest) == sm.source
+```
+"""
 function source(::AbstractHsmStateMachine) end
-function source!(::AbstractHsmStateMachine, state::StateType) end
+
+"""
+    source!(sm::AbstractHsmStateMachine, state::StateType)
+
+Set source state of state machine `sm` to `state`.
+
+# Implementation Example
+```julia
+Hsm.source!(sm::HsmTest, state) = sm.source = state
+```
+"""
+function source!(::AbstractHsmStateMachine, ::StateType) end
+
+"""
+    event(sm::AbstractHsmStateMachine)
+
+Get event of state machine `sm`.
+
+# Implementation Example
+```julia
+Hsm.event(sm::HsmTest) == sm.event
+```
+"""
 function event(::AbstractHsmStateMachine) end
 
-const root() = :Root
-initialize(sm::AbstractHsmStateMachine) = (current!(sm, root()); source!(sm, root()))
+"""
+    initialize!(sm::AbstractHsmStateMachine)
 
-@valsplit function ancestor(sm::AbstractHsmStateMachine, Val(state::StateType))
-    @error("No ancestor for state $state")
-    return root()
+Initialize state machine `sm`.
+"""
+function initialize!(sm::AbstractHsmStateMachine)
+    current!(sm, Root)
+    source!(sm, Root)
+    on_initial!(sm, Root)
 end
 
+"""
+    ancestor(sm::AbstractHsmStateMachine, state::Val{STATE})
+
+Get ancestor (superstate) of `state` in state machine `sm`. Ensure the top most state
+ancestor is [`Root`](@ref).
+
+# Implementaiton Example
+To speficfy the ancestor for `sm`=HsmState State_S is [`Root`](@ref):
+```julia
+Hsm.ancestor(sm::HsmTest, ::Val{State_S}) = Root
+```
+"""
+@valsplit function ancestor(sm::AbstractHsmStateMachine, Val(state::StateType))
+    @error "No ancestor for state $state"
+    return Root
+end
+
+"""
+    on_initial!(sm::AbstractHsmStateMachine, state::Val{STATE})
+
+Handle initial transition to `state` in state machine `sm`.
+
+Initial transitions must transition to a child state of `state` or return [`EventHandled`](@ref)
+when the transition is complete.
+
+# Example
+```julia
+function Hsm.on_initial!(sm::HsmTest, state::Val{Top})
+    handled = transition!(sm, State_S2) do
+        # Do something on the transition
+        sm.foo = 0
+    end
+    return handled
+end
+```
+or
+```julia
+Hsm.on_initial!(sm::HsmTest, state::Val{State_S}) =
+    Hsm.transition!(sm, State_S11) do
+        # Do something on the transition
+    end
+```
+"""
 @valsplit on_initial!(sm::AbstractHsmStateMachine, Val(state::StateType)) = EventHandled
+
+"""
+    on_entry!(sm::AbstractHsmStateMachine, state::Val{STATE})
+
+Entry action for `state` in state machine `sm`.
+
+# Example
+```julia
+function Hsm.on_entry!(sm::HsmTest, state::Val{State_S2})
+    # Do something on entry
+end
+```
+"""
 @valsplit on_entry!(sm::AbstractHsmStateMachine, Val(state::StateType)) = nothing
+
+"""
+    on_exit!(sm::AbstractHsmStateMachine, state::Val{STATE})
+
+Exit action for `state` in state machine `sm`.
+
+# Example
+```julia
+function Hsm.on_exit!(sm::HsmTest, state::Val{State_S2})
+    # Do something on exit
+end
+```
+"""
 @valsplit on_exit!(sm::AbstractHsmStateMachine, Val(state::StateType)) = nothing
 
-# Generic on_event! handler for unhandled events
+"""
+    on_event!(sm::AbstractHsmStateMachine, state::Val{STATE}, event::Val{:EVENT})
+
+Handle `event` in `state` of state machine `sm`.
+
+Return [`EventHandled`](@ref) if `event` was handled or [`EventNotHandled`](@ref) if `event` was not handled.
+
+# Example
+To define an event handler for `sm`=HsmTest, state=State_S1, `event`=:Event_D 
+```julia
+function Hsm.on_event!(sm::HsmTest, state::Val{State_S1}, event::Val{:Event_D})
+    if sm.foo == 0
+        return Hsm.transition!(sm, State_S1) do
+            sm.foo = 0
+        end
+    else
+        return EventNotHandled
+    end
+end
+```
+Or if the `event` is an internal transition which does not change the state:
+```julia
+function Hsm.on_event!(sm::HsmTest, state::Val{State_S2}, event::Val{:Event_I})
+    if sm.foo == 0
+        sm.foo = 1
+        return EventHandled
+    else
+        return EventNotHandled
+    end
+end
+```
+"""
 @valsplit function on_event!(
     sm::AbstractHsmStateMachine,
     Val(state::StateType),
     Val(event::Symbol),
 )
-    if state == root()
+    # Events are considered handled if they reach the root state
+    if state == Root
         return EventHandled
     end
     return EventNotHandled
@@ -54,8 +292,24 @@ function do_exit!(sm, s, t)
     return
 end
 
+"""
+    transition!(sm::AbstractHsmStateMachine, t)
+    transition!(action::Function, sm::AbstractHsmStateMachine, t)
+
+Transition state machine `sm` to state `t`.
+The `action` function will be called, if specified, during the transition when the main source state has
+    exited before entering the target state. [`transition!`](@ref) will always return [`EventHandled`](@ref).
+
+# Example
+```julia
+transition!(sm, State_S2) do
+    # Do something on the transition
+    sm.foo = 0
+end
+```
+"""
 function transition!(sm::AbstractHsmStateMachine, t)
-    return transition!(Returns(nothing), sm, t)
+    transition!(Returns(nothing), sm, t)
 end
 
 function transition!(action::Function, sm::AbstractHsmStateMachine, t)
@@ -76,15 +330,24 @@ function transition!(action::Function, sm::AbstractHsmStateMachine, t)
     current!(sm, t)
     source!(sm, t)
 
-    return on_initial!(sm, t)
+    on_initial!(sm, t)
 end
 
-# Is 'a' an ancestor of 'b'
+"""
+    isancestorof(sm::AbstractHsmStateMachine, a, b)
+
+Check if state `a` is an ancestor (superstate) of state `b` in state machine `sm`.
+
+# Example
+```julia
+false == isancestorof(sm, State_S1, State_S2)
+```
+"""
 function isancestorof(sm, a, b)
-    if a == root()
+    if a == Root
         return false
     end
-    while b != root()
+    while b != Root
         if a == b
             return true
         end
@@ -93,13 +356,20 @@ function isancestorof(sm, a, b)
     return false
 end
 
+"""
+    find_lca(sm::AbstractHsmStateMachine, s, t)
+
+Find the least common ancestor of states `s` and `t` in state machine `sm`.
+
+Returns the least common ancestor of `s` and `t` or [`Root`](@ref) if no common ancestor is found.
+"""
 function find_lca(sm, s, t)
     # Handle case where main source is equal to target
     if s == t
         return ancestor(sm, s)
     end
 
-    while s != root() && t != root()
+    while s != Root && t != Root
         if s == t
             return s
         elseif isancestorof(sm, s, t)
@@ -108,9 +378,14 @@ function find_lca(sm, s, t)
             s = ancestor(sm, s)
         end
     end
-    return root()
+    return Root
 end
 
+"""
+    dispatch!(sm::AbstractHsmStateMachine)
+
+Dispatch the event in state machine `sm`.
+"""
 function dispatch!(sm::AbstractHsmStateMachine)
     s = current(sm)
     e = event(sm)
@@ -124,9 +399,11 @@ function dispatch!(sm::AbstractHsmStateMachine)
     end
 end
 
-export AbstractHsmState, AbstractHsmMachine
+export AbstractHsmStateMachine
+export EventHandled, EventNotHandled, Root
+export current, current!, source, source!, event, ancestor
+export initialize!
 export on_initial!, on_entry!, on_exit!, on_event!
 export transition!, dispatch!
-export EventHandled, EventNotHandled, root
 
 end # module Hsm
